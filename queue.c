dbd652c68758a94f29aed2c75dca48f31398fe9f
PlusThousand0107 <PlusThousand0107@gmail.com>
2023-02-28 21:48:08 +0800

    Implement 'q_delete_dup'

--- queue.c
+++ queue.c
@@ -122,6 +122,28 @@ bool q_delete_mid(struct list_head *head)
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head)
+        return false;
+
+    LIST_HEAD(trash);
+    element_t *it, *safe;
+    struct list_head *cut = head;
+
+    list_for_each_entry_safe (it, safe, head, list) {
+        if (&safe->list != head && strcmp(safe->value, it->value) == 0)
+            continue;
+
+        if (it->list.prev != cut) {
+            LIST_HEAD(temp);
+            list_cut_position(&temp, cut, &it->list);
+            list_splice(&temp, &trash);
+        }
+        cut = safe->list.prev;
+    }
+
+    list_for_each_entry_safe (it, safe, &trash, list)
+        q_release_element(it);
+
     return true;
 }
 
